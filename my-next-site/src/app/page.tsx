b94076105de576da2ae40efd63aafd848946b6ad
mps827 <155172170+mps827@users.noreply.github.com>
2024-06-03 15:47:14 +0330

    Add files via upload

---
+++ my-next-site/src/app/page.tsx
@@ -0,0 +1,13 @@
+import MainLayout from "@/src/view/layout/MainLayout";
+import styles from "./styles/page.module.css";
+
+export default function Home() {
+  return (
+    <MainLayout>
+      <center>
+        <h1>Main Page</h1>
+        <h4>ComingSoon</h4>
+      </center>
+    </MainLayout>
+  );
+}
